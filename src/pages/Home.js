82faba5596e192ac0a1cdf8ebd9c8b4ce582c6a2
Shivam Shah <shiva.sean27@gmail.com>
2023-01-16 23:47:02 +0530

    adding google logo in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,33 +4,31 @@ import './Home.css';
 import AppsIcon from '@material-ui/icons/Apps';
 import { Avatar } from '@material-ui/core';
 import Search from '../Search';
+import googleLogo from '../../src/GoogleLogo.png';
 
 function Home() {
-  return (
-    <div>
-      <div className='home_header'>
-        <div className='home_headerleft'>
-          <Link to='about'>About</Link>
-          <Link to='store'>Store</Link>
-        </div>
-        <div className='home_headerRight'>
-          <Link to='gmail'>Gmail</Link>
-          <Link to='images'>Images</Link>
-          <AppsIcon />
-          <Avatar />
-        </div>
-      </div>
-      <div className='home_body'>
-        <img
-          src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c51f.png'
-          alt='logo'
-        />
-        <div className='home_inputContainer'>
-          <Search />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className='home_header'>
+				<div className='home_headerleft'>
+					<Link to='about'>About</Link>
+					<Link to='store'>Store</Link>
+				</div>
+				<div className='home_headerRight'>
+					<Link to='gmail'>Gmail</Link>
+					<Link to='images'>Images</Link>
+					<AppsIcon />
+					<Avatar />
+				</div>
+			</div>
+			<div className='home_body'>
+				<img src={googleLogo} alt='logo' />
+				<div className='home_inputContainer'>
+					<Search />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
